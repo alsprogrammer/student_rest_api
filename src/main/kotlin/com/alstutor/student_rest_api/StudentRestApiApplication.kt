9c0df4bd33156d67fa049fd81d2736215df58b41
Александр <alstutor@gmail.com>
2018-09-01 19:31:27 +0700

    The first commit

---
+++ src/main/kotlin/com/alstutor/student_rest_api/StudentRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.alstutor.student_rest_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentRestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudentRestApiApplication>(*args)
+}
