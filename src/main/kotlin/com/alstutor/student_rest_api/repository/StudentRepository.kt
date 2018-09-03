e520618d80bdc6c66c24ced8d9c077a1ee79f7a6
alsprogrammer <alstutor@gmail.com>
2018-09-03 10:47:49 +0700

    The query for a student by name has been implemented

--- src/main/kotlin/com/alstutor/student_rest_api/repository/StudentRepository.kt
+++ src/main/kotlin/com/alstutor/student_rest_api/repository/StudentRepository.kt
@@ -5,5 +5,5 @@ import com.alstutor.student_rest_api.model.Student
 import org.springframework.data.repository.CrudRepository
 
 interface StudentRepository : CrudRepository<Student, String> {
-    fun findByFirstnameAndSurnameAndLastnameIgnoreCase(fistName: String, surName: String, lastName: String): List<Student>
+    fun findByLastNameAndFirstNameAndSurNameIgnoreCase(lastName: String, firstName: String, surName: String): List<Student>
 }
