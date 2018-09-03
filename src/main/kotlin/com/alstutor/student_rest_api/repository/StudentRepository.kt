f190aec2556e3ea2aeb2a3f21c047c2a2b2598f8
alsprogrammer <alstutor@gmail.com>
2018-09-03 11:16:14 +0700

    Repository queries have been implemented

--- src/main/kotlin/com/alstutor/student_rest_api/repository/StudentRepository.kt
+++ src/main/kotlin/com/alstutor/student_rest_api/repository/StudentRepository.kt
@@ -1,9 +1,13 @@
 package com.alstutor.student_rest_api.repository
 
 import com.alstutor.student_rest_api.model.Student
+import com.alstutor.student_rest_api.model.Group
 
 import org.springframework.data.repository.CrudRepository
 
 interface StudentRepository : CrudRepository<Student, String> {
-    fun findByLastNameAndFirstNameAndSurNameIgnoreCase(lastName: String, firstName: String, surName: String): List<Student>
+    fun getById(id: String): List<Student>
+    fun findByLastNameAndFirstNameAndSurNameAllIgnoreCase(lastName: String, firstName: String, surName: String): List<Student>
+    fun findByLastNameAndFirstNameAndSurNameAllIgnoreCaseAndGroup(lastName: String, firstName: String, surName: String): List<Student>
+    fun findByGroupOrderByLastNameAscFirstNameAscSurNameAsc(group: Group)
 }
