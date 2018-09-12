bbb35ae6d0be0a59251775b7d5ab419f488c75cf
Александр <alstutor@gmail.com>
2018-09-12 22:22:55 +0700

    The test implementation is in progress.

--- src/main/kotlin/com/alstutor/studentrestapi/service/StudentService.kt
+++ src/main/kotlin/com/alstutor/studentrestapi/service/StudentService.kt
@@ -3,6 +3,7 @@ package com.alstutor.studentrestapi.service
 import com.alstutor.studentrestapi.model.Student
 import com.alstutor.studentrestapi.model.Group
 import com.alstutor.studentrestapi.repository.StudentRepository
+import com.alstutor.studentrestapi.repository.GroupRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -11,12 +12,17 @@ interface StudentService {
     fun studentByName(lastName: String, firstName: String, surName: String): List<Student>
     fun studentByNameAndGroup(lastName: String, firstName: String, surName: String, group: Group): List<Student>
     fun studentsOfGroup(group: Group): List<Student>
+    fun groupById(id: String): Group
+    fun groupsByCreationYear(year: Int): List<Group>
+    fun groupsByTerm(term: Int): List<Group>
+    fun groupsByYearAndTerm(year: Int, term: Int): List<Group>
 }
 
 @Service("StudentService")
 class StudentServiceImpl : StudentService {
     @Autowired
     lateinit var studentRepository: StudentRepository
+    lateinit var groupRepository: GroupRepository
 
     override fun studentById(id: String): Student = studentRepository.getById(id)
     override fun studentByName(lastName: String, firstName: String, surName: String): List<Student> = 
@@ -25,4 +31,8 @@ class StudentServiceImpl : StudentService {
                     studentRepository.findByLastNameAndFirstNameAndSurNameAllIgnoreCaseAndGroup(lastName, firstName, surName, group)
     override fun studentsOfGroup(group: Group): List<Student> = 
                     studentRepository.findByGroupOrderByLastNameAscFirstNameAscSurNameAsc(group)
+    override fun groupById(id: String): Group = groupRepository.getById(id)
+    override fun groupsByCreationYear(year: Int): List<Group> = groupRepository.findByCreationYear(year)
+    override fun groupsByTerm(term: Int): List<Group> = groupRepository.findByTerm(term)
+    override fun groupsByYearAndTerm(year: Int, term: Int): List<Group> = groupRepository.findByCreationYearAndTerm(year, term)
 }
---
+++ src/test/kotlin/com/alstutor/studentrestapi/service/StudentServiceTest.kt
@@ -0,0 +1,29 @@
+package com.alstutor.studentrestapi.service
+
+import com.alstutor.studenttestapi.StudentServiceFongoTest
+import com.alstutor.studentrestapi.model.Student
+import com.alstutor.studentrestapi.model.StudentInfo
+import com.alstutor.studentrestapi.model.Group
+import com.alstutor.studentrestapi.model.GroupInfo
+import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
+
+class StudentServiceTest : StudentServiceFongoTest() {
+    @Autowired
+    lateinit var studentService: StudentService
+
+    @Test
+    fun testGroups() {
+        logger.info("Begin with groups")
+        val firstTermGroups: List<Group> = studentService.groupsByTerm(1)
+        assertEquals(firstTermGroups.size, 2, "There should be 2 groups of the fisrt term")
+    }
+
+    companion object {
+        val logger: Logger =
+                LoggerFactory.getLogger(StudentServiceTest::class.java)
+    }
+}
